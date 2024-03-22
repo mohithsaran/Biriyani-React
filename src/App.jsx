544682f3506d1529ff55366d8cdc20747f404bfd
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-22 13:57:33 +0530

    Biriyani Picture was added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Ingre from './Ingredients'
 import Error from './Error'
+import pb from "./assets/Paneer-Biriyani.jpg"
 import './App.css'
 
 function App() {
@@ -13,7 +14,8 @@ const ing=["Basmati Rice","Oil","Tomato","Onion","Potato","Carrot","Panneer"]
    
     <h3> Biriyani Ingredients </h3>
     <Error ingredients={ing}/>
-    <Ingre ingredients={ing}/>
+    <Ingre ingredients={ing}/><br/>
+    <img src={pb} alt="Paneer Biriyani" width={300} height={400}/>
     
     </>
   )
